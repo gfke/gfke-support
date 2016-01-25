5fb1b547c4e552903fb56eae310922927d691645
Nax <m.neundorf@live.de>
2016-01-25 15:09:33 +0000

    cjs -> es6 behaivor fix

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import Middlewares from './middlewares/index.js';
 import Services from './services/index.js';
 import Utils from './utils/index.js';
 
-export default {Middlewares, Services, Utils};
+module.exports = {Middlewares, Services, Utils};
