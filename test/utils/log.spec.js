9e9535fd6c37dd608cefc28e6ddb17b0930ac29f
Stefan Panitz <info@stefanpanitz.de>
2018-07-11 16:12:18 +0200

    add tests

--- test/utils/log.spec.js
+++ test/utils/log.spec.js
@@ -1,5 +1,40 @@
 'use strict';
 
+var raven = require('raven');
+
 describe('Util: log', function () {
-  it("spec not implemented yet");
+  describe('Method simpleLogger', function () {
+    it('should instatiate pino with a stdout stream without options', function() {
+      var logger = this.lib.utils.log.simpleLogger();
+      logger.stream.should.be.a.instanceOf(process.stdout.constructor);
+    });
+    it('should instatiate pino with a raven stream when activated', function() {
+      var opts = {
+        raven: {
+          active: true,
+          ravenDsn: 'https://username:password@example.com/1'
+        }
+      }
+      var logger = this.lib.utils.log.simpleLogger(opts);
+      logger.stream.should.be.a.instanceOf(this.lib.utils.log.RavenStream);
+    });
+    it('should instatiate pino with custom log level', function() {
+      var opts = {
+        pino: {
+          level: 'warn'
+        }
+      }
+      var logger = this.lib.utils.log.simpleLogger(opts);
+      logger._levelVal.should.equal(40)
+    });
+    it('should instatiate pino with bunyan opts', function() {
+      var opts = {
+        bunyan: {
+          level: 'error'
+        }
+      }
+      var logger = this.lib.utils.log.simpleLogger(opts);
+      logger._levelVal.should.equal(50)
+    });
+  });
 });
